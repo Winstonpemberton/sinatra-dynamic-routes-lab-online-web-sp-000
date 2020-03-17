19a16f2d7c1bd8f1914deecbe9c9fe0c516b444d
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-17 23:25:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    
+
     case @operation
     when "add"
       result = (@number1 + @number2).to_s
