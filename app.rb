aa793023f35f0837d6a74f8b9f203217479e929a
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-17 23:00:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    loop.times(@number) do
+    @number.times do
       puts "@phrase"
     end
     binding.pry
